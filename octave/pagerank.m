9b148482008e532b37e3417e39607f8bb92a16e7
Josh Devins <josh@soundcloud.com>
2013-04-26 17:30:16 +0200

    Refactored, mostly working

--- octave/pagerank.m
+++ octave/pagerank.m
@@ -1,46 +1,49 @@
-% based on code from Wikipedia: http://en.wikipedia.org/wiki/PageRank
+%%
+%% PageRank
+%%
 
-% uniform priors, no prior solution
-function [v] = pagerank(A, alpha, v_quadratic_error_threshold)
+% PageRank with uniform priors and no previous solution
+function [v, num_iters] = unbiased_pagerank(A, alpha, v_quadratic_error_threshold, max_iters)
 
-n = size(A, 2); % number of nodes in the graph 
-uniform_priors = % TODO
-for i = 1:n, x(i) = 1 / n; end
+  n = size(A, 1); % number of nodes in the graph
+  s = ones(n, 1) / n;
 
-pagerank(A, alpha, uniform_prior, starting_v, v_quadratic_error_threshold)
+  % uniform prior: 1/n
+  % uniform starting solution: 1/n
+  % both are the same so just use s for both
+
+  [v, num_iters] = pagerank(A, alpha, s, s, v_quadratic_error_threshold, max_iters);
 endfunction
 
-function [v] = pagerank(A, alpha, priors, v, v_quadratic_error_threshold)
+% PageRank with specific priors vector and a previous solution
+function [v, num_iters] = pagerank(A, alpha, priors, v, v_quadratic_error_threshold, max_iters)
 
-% scalars
-n = size(A, 2); % number of nodes in the graph
+  % scalars
+  n = size(A, 1); % number of nodes in the graph
 
-% matrices
-M = norm(A, 1); % L1 norm
-P = M.';
+  % matrices
+  sum_vector = sum(A, 2); % col vector with values are sum of rows
 
-% vectors
-% build dangling nodes vector, where entry is 1 when node is dangling, 0 otherwise
-d = max(P, [], 1); % dangling nodes will be 0 here, others will be non-0
-for i = 0:n
-  if (d(i) == 0)
-    d(i) = 1;
-  else
-    d(i) = 0;
-  endif
-endfor
+  % use eps to ensure dangling rows don't kill this
+  M = spdiags(1 ./ (eps + sum_vector), 0, n, n) * A; % L1 row normalised
+  P = M';
 
-ones = ones(n, n);
+  % vectors
+  d = sum_vector < eps; % dangling nodes vector where d(i) is 1 for dangling node
+  p = alpha * priors;
 
-% constants
-P_hat = (1 - alpha) .* (P - diag((1 / n - 1) .* d));
+  % derived constants
+  P_hat = (1 - alpha) * (P - diag((1 / n - 1) * d));
 
-% iterate
-% use L2 norm of diff between solution vectors to check for stopping criteria
-while(norm(v - last_v, 2) > v_quadratic_error_threshold)
-  last_v = v;
-  mu = (1 - alpha) .* ((1 / n - 1) .* d.') .* last_v + (alpha * 1 / n)
-  v = P_hat .* last_v + mu .* ones;
-end
- 
+  % iterate
+  % use L2 norm of diff between solution vectors to check for stopping criteria
+  for num_iters = 1:max_iters
+    last_v = v;
+    mu = ((1 - alpha) / (n - 1)) * (d' * last_v);
+    v = (P_hat * last_v) + mu + p;
+
+    if norm(v - last_v, 2) < v_quadratic_error_threshold
+      return;
+    end
+  end
 endfunction
